1b1903819e86973594b3f6b9576829463e1e2070
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2025-12-08 06:11:11 +0000

    V6.01 Code
    Completed Remodeled Security. Checks for Already Loggedin.

--- src/components/Homebar/homebar.tsx
+++ src/components/Homebar/homebar.tsx
@@ -53,7 +53,7 @@ export default function Homebar({ numItems }: HomebarProps) {
             cursor: 'pointer',
           }}
         >
-          [CG.UI.V4.3]
+          [CG.UI.V6.01]
         </Typography>
 
         {/* Spacer */}
